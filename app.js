cf1b104bfe5e909204bdf6b1c0e901fe0ed59efd
Chelsea Ehimare <chelseaehimare01@gmail.com>
2023-01-09 19:58:14 +0000

    added topics

--- app.js
+++ app.js
@@ -4,19 +4,17 @@ const app = express()
 
 app.use(cors())
 
-
-
 app.get('/lessons', (request, response)=> {
     response.json(
       [
-        {topic:"math", location:"Hendon",price:100}
-       ,{topic:"math", location:"Colindale",price:80}
-       ,{topic:"math", location:"Brent Cross",price:90}
-       ,{topic:"math", location:"Golders Green",price:120}]
+        {topic:"mathematics", location:"Hendon",price:100}
+       ,{topic:"English", location:"Colindale",price:80}
+       ,{topic:"French", location:"Brent Cross",price:90}
+       ,{topic:"Igbo", location:"Golders Green",price:120}]
     )
 })
 
-app.get('/users', (request, response)=> {
+app.get('/user', (request, response)=> {
     response.json(
         [
             {
